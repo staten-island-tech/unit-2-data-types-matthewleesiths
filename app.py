da19e301db25b76c7301247a3e543c7c68086f27
matthewleesiths <matthewl497@nycstudents.net>
2024-09-13 09:21:41 -0400

    unit 2
    start

---
+++ app.py
@@ -0,0 +1,13 @@
+""" food = input("Do you like Chicken?")
+if food == "yes" or "Yes" or "Yep" or "yep" or "Ye" or "ye" or "Definitely" or "definitely":
+    print("correct")
+else:
+    print("incorrect") """
+
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
