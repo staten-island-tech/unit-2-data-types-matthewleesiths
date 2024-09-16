e7c54c3c07f7fd4f15a8b9fbec0da3235297a568
matthewleesiths <matthewl497@nycstudents.net>
2024-09-16 09:20:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,28 @@ if food == "yes" or "Yes" or "Yep" or "yep" or "Ye" or "ye" or "Definitely" or "
 else:
     print("incorrect") """
 
-temp = 75
+""" x = int(3)
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6]) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+""" x = "test"
+print(f"hello {x}")  """
+
+temp = input("what is the temperature?")
 if temp > 68:
     print('warm')
 elif temp == 68:
